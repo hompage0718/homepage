63bb978cf91f3cd639bc965d3d2b043541f50cd5
hompage0718 <johnkobz14@gmail.com>
2021-07-25 23:01:48 +0900

    initial 3

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ dotenv.config({ path: "./config/.env.development.local" });
 
 connectDB();
 
-const user = require("./routes/user");
+// const user = require("./routes/user");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/hp/user/", user);
+// app.use("/api/hp/user/", user);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
