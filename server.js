67f469ddb23c2c4c15b103e0425e9de5d3c0e798
hompage0718 <johnkobz14@gmail.com>
2021-07-26 09:52:23 +0900

    initial 4

--- server.js
+++ server.js
@@ -7,17 +7,21 @@ dotenv.config({ path: "./config/.env.development.local" });
 
 connectDB();
 
-// const user = require("./routes/user");
-
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.use("/api/hp/user/", user);
+// Define Routes
+const user = require("./routes/user");
 
+app.use("/api/hp/user/", user);
+
+// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
-
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
